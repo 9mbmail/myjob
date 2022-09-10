423b85712c9f32d9e1f3e6ffcd4b17b7f3519593
Manoj Kumar <113238075+9mbmail@users.noreply.github.com>
2022-09-10 16:41:21 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,1 +1,8 @@
-print("hello")
+import mysql.connector
+try:
+    mydb = mysql.connector.connect(host="Mylocal",user="9mbmail",password="9A^37EVmdH@F49Euc")
+
+    if mydb.is_connected():
+        print("Connected")
+except:
+    print("Unable to connect")
